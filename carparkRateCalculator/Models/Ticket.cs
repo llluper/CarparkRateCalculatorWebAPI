ce4b7de1d9752fd4c5e414e2c50820945766b77a
llluper <mitchelljstanford@gmail.com>
2019-05-13 16:32:02 +1000

    Create Ticket model

---
+++ carparkRateCalculator/Models/Ticket.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace carparkRateCalculator.Models
+{
+    public class Ticket
+    {
+        public DateTime entry { get; set; }
+        public DateTime exit { get; set; }
+    }
+}
